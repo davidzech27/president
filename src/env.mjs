034f70deef7e9e00355cc00feb3d3d8569425455
David Zechowy <davidzech27@gmail.com>
2023-10-20 17:34:17 -0700

    add jwt secret

--- src/env.mjs
+++ src/env.mjs
@@ -4,6 +4,7 @@ import { createEnv } from "@t3-oss/env-nextjs"
 const env = createEnv({
 	server: {
 		NODE_ENV: z.enum(["development", "test", "production"]),
+		JWT_SECRET: z.string(),
 		DATABASE_URL: z.string().url(),
 		DATABASE_AUTH_TOKEN: z.string(),
 		OPENAI_SECRET_KEY: z.string(),
@@ -17,6 +18,7 @@ const env = createEnv({
 	},
 	runtimeEnv: {
 		NODE_ENV: process.env.NODE_ENV,
+		JWT_SECRET: process.env.JWT_SECRET,
 		DATABASE_URL: process.env.DATABASE_URL,
 		DATABASE_AUTH_TOKEN: process.env.DATABASE_AUTH_TOKEN,
 		OPENAI_SECRET_KEY: process.env.OPENAI_SECRET_KEY,
