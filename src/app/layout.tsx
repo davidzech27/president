ebb1e7c3c4a191b201b9a3d3f3d6f38e0cc1b5dd
David Zechowy <davidzech27@gmail.com>
2023-10-20 17:32:47 -0700

    fix lint error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css"
 export const metadata = {
 	title: "president game",
 	description: "ai-powered presidential election simulator",
-	metadataBase: new URL("https://highchat.vercel.app"),
+	metadataBase: new URL("https://presidentgame.vercel.app"),
 }
 
 export default function RootLayout({
