d6c1a82ffeae42c620443cb39af27273e031098f
David Zechowy <davidzech27@gmail.com>
2023-11-03 13:18:57 -0700

    stop using websockets

--- src/update/useSubscribeToGameUpdates.ts
+++ src/update/useSubscribeToGameUpdates.ts
@@ -1,6 +1,7 @@
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
-import useRealtime from "~/realtime/useRealtime"
+// import useRealtime from "~/realtime/useRealtime"
 
 export default function useSubscribeToGameUpdates({
 	gameId,
@@ -9,9 +10,17 @@ export default function useSubscribeToGameUpdates({
 }) {
 	const router = useRouter()
 
-	useRealtime({
-		channel: gameId,
-		event: "update",
-		onMessage: () => router.refresh(),
-	})
+	// useRealtime({
+	// 	channel: gameId,
+	// 	event: "update",
+	// 	onMessage: () => router.refresh(),
+	// })
+
+	useEffect(() => {
+		const intervalId = setInterval(() => router.refresh(), 1000)
+
+		return () => {
+			clearInterval(intervalId)
+		}
+	}, [router])
 }
